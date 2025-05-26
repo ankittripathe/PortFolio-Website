b465789b4b7967c8027c57797afcb13558fd57fb
Ankit Tripathi <ankittripathi.dev@gmail.com>
2025-05-26 13:17:41 +0545

    initial commit

---
+++ src/Components/Projects.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ProjectCard from "./ProjectCard";
+import ProjecctApi from "../API/Project-api.js";
+
+function Projects() {
+  console.log(ProjecctApi[0]);
+  return (
+    <>
+      <section className="Projects" id="Projects">
+        <div className="main-container">
+          <div className="project-info-container">
+            <div>
+              <p className="project-heading">PORTFOLIO</p>
+              <h3>Each project is a unique piece of development 🧩</h3>
+            </div>
+            <ProjectCard index={ProjecctApi[0]}/>
+            <ProjectCard index={ProjecctApi[1]}/>
+            <ProjectCard index={ProjecctApi[2]}/>
+            <ProjectCard index={ProjecctApi[3]}/>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Projects;
