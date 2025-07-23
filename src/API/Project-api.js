32ca19ca90cf65a5c8919762c283bd7fbc56e212
Ankit Tripathi <ankitdhonee@gmail.com>
2025-07-23 13:34:35 +0530

    updated

--- src/API/Project-api.js
+++ src/API/Project-api.js
@@ -1,17 +1,17 @@
-import trendWear from '../assets/trendwear.png'
-import nexusHome from '../assets/nexushome.png'
-import countDown from '../assets/countdown.png'
-import studySync from '../assets/studysync.png'
+import trendWear from "../assets/trendwear.png";
+import nexusHome from "../assets/nexushome.png";
+import countDown from "../assets/countdown.png";
+import studyWell from "../assets/studywell.png";
 
 const ProjecctApi = [
   {
     img: trendWear,
     heading: "trendWear",
-    para: "TrendWear is a modern online fashion hub where users can effortlessly browse, discover, and shop trendy clothing from top brands.",
+    para: "TrendWear is a modern online fashion destination where users can effortlessly browse, discover, and shop the latest trendy clothing from top brands.",
     tech1: "React",
     tech2: "React-Router",
-    tech3: "React-Icons",
-    tech4: "MUI",
+    tech3: "MUI",
+    tech4: "React-Icons",
     code: "https://github.com/ankittripathe/TrendWear",
     live: "https://trendwear-clothingsite.vercel.app/",
   },
@@ -19,35 +19,35 @@ const ProjecctApi = [
   {
     img: nexusHome,
     heading: "NexusHome",
-    para: "CodeFusion is a real-time code syncing application where people can contribute code in real-time. CodeFusion can overcome the sharing of large data files.",
+    para: "NexusHome is a responsive real estate web app built with React, Tailwind CSS and Framer-Motion offering a smooth user experience for browsing properties and connecting with agents on any device.",
     tech1: "React",
     tech2: "Tailwind",
-    tech3: "React-Router",
-    tech4: 'Framer Motion',
+    tech3: "Framer-Motion",
+    tech4: "React-Router",
     code: "https://github.com/ankittripathe/RealEstate-App",
-    live: "https://nexushome-realestate-project.vercel.app/",
+    live: "https://nexushome-realstate.vercel.app/",
   },
 
   {
     img: countDown,
     heading: "CountDown Timer",
-    para: "DevFinder Application for GitHub user profile summaries, benefiting recruiters, project managers, and developers alike.",
+    para: "Countdown Timer is a sleek and responsive web app that accurately displays the time left until a selected date using real-time updates with HTML, CSS, and JavaScript.",
     tech1: "Html",
-    tech2: "Css",
+    tech2: "CSS",
     tech3: "Javascript",
     code: "https://github.com/ankittripathe/CountDown-Timer",
     live: "https://countdowntimer-project.netlify.app/",
   },
 
   {
-    img: studySync,
-    heading: "StudySync LearningApp🎬",
-    para: "MoviesApp shows the movies description & information, also provide the searching functionality.",
+    img: studyWell,
+    heading: "StudyWell LearningApp",
+    para: "StudyWell is a smart, responsive learning app offering personalized lessons, quizzes, and progress tracking to help students learn efficiently on any device.",
     tech1: "Html",
-    tech2: "Css",
+    tech2: "CSS",
     tech3: "Javascript",
-    code: "https://github.com/ankittripathe/StudySync",
-    live: "https://ankittripathe.github.io/StudySync/",
+    code: "https://github.com/ankittripathe/StudyWell",
+    live: "https://studywell-webapp.netlify.app/",
   },
 ];
 
