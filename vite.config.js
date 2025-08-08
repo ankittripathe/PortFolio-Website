66aeed228a3a25596f3ab73a0ca806af08fe4613
Ankit Tripathi <ankitdhonee@gmail.com>
2025-08-08 20:07:29 +0530

    final version

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    host: true, // Exposes to local network
+    port: 5173, // Optional: set your own port
+  },
 })
