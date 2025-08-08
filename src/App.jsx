44e2ef0ca6ee256e0601be675a1556161c15f96b
Ankit Tripathi <ankitdhonee@gmail.com>
2025-08-08 13:58:53 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Projects from "./Components/Projects";
 import Contact from "./Components/Contact";
 import Footer from "./Components/Footer";
 
-const App =()=> {
+const App = () => {
   return (
     <>
       <Header />
@@ -16,6 +16,6 @@ const App =()=> {
       <Footer />
     </>
   );
-}
+};
 
 export default App;
