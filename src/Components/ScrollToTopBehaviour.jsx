42ef532273e848ddaf0871c4abd2535540b4e86c
Ankit Tripathi <ankittripathi.dev@gmail.com>
2025-08-18 13:09:29 +0530

    scroll to top

--- src/Components/ScrollToTopBehaviour.jsx
+++ src/Components/ScrollToTopBehaviour.jsx
@@ -29,7 +29,23 @@ const ScrollToTopBehaviour = () => {
   return (
     visible && (
       <button onClick={scrollToTop} className="scrollToTop">
-        <FaArrowUp size={24} />
+        {/* <FaArrowUp size={24} /> */}
+        <div className="arrowbtn">
+          <span>Top</span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            stroke="white"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            viewBox="0 0 24 24"
+          >
+            <path d="M12 4 L6 16 L12 12 L18 16 Z" />
+          </svg>
+        </div>
       </button>
     )
   );
