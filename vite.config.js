c9582336afa82f864ae3b33e7698a75ecdbc6e00
Ankit tripathi <ankittripathi2k24@gmail.com>
2026-02-04 15:41:15 +0530

    version updated

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +8,4 @@ export default defineConfig({
     host: true, // Exposes to local network
     port: 5173, // Optional: set your own port
   },
-})
+});
